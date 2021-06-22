5d7fccbdf4a26b0eeadb6d6c665e62f1a8a4b335
ShizuBk <86270775+ShizuBk@users.noreply.github.com>
2021-06-21 20:00:18 -0500

    Ya lleva a la parte de los cursos, ahora se necesita que lleve a la parte de las lecciones

--- app/src/main/java/com/example/lernnin2/ui/cursos/CursosFragment.java
+++ app/src/main/java/com/example/lernnin2/ui/cursos/CursosFragment.java
@@ -1,39 +1,42 @@
 package com.example.lernnin2.ui.cursos;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
 import com.example.lernnin2.R;
-import com.example.lernnin2.databinding.FragmentCursosBinding;
 import com.example.lernnin2.ui.lecciones.fb.CFb;
 
 
-public class CursosFragment extends Fragment implements View.OnClickListener {
+public class CursosFragment extends Fragment{
     public CursosFragment(){
 
     }
 
     Button btnCFb;
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceOf){
-        return inflater.inflate(R.layout.fragment_cursos,container,false);
 
-        View v;
-        v = inflater.inflate(R.layout.fragment_fb,container,false);
+        view=inflater.inflate(R.layout.fragment_cursos,container,false);
 
-
+        btnCFb=(Button) view.findViewById(R.id.buttonFB);
+        
+        btnCFb.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(), CFb.class);
+                startActivity(intent);
+            }
+        });
+        
+        return view;
     }
 
 
--- app/src/main/java/com/example/lernnin2/ui/lecciones/fb/CFb.java
+++ app/src/main/java/com/example/lernnin2/ui/lecciones/fb/CFb.java
@@ -1,9 +1,11 @@
 package com.example.lernnin2.ui.lecciones.fb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,6 +13,11 @@ import com.example.lernnin2.R;
 
 public class CFb extends Fragment {
     public CFb(){
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceOf){
+        return inflater.inflate(R.layout.fragment_cursos,container,false);
 
     }
 
