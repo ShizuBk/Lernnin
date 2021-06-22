94fa6640e48b62d89447162c3f6b4dcfcd5fce92
ShizuBk <86270775+ShizuBk@users.noreply.github.com>
2021-06-21 20:45:18 -0500

    Intenté que jalara, pero nel

--- app/src/main/java/com/example/lernnin2/ui/lecciones/fb/CFb.java
+++ app/src/main/java/com/example/lernnin2/ui/lecciones/fb/CFb.java
@@ -12,13 +12,28 @@ import androidx.fragment.app.Fragment;
 import com.example.lernnin2.R;
 
 public class CFb extends Fragment {
+
     public CFb(){
 
     }
+
+    View view;
+    Button btnL1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceOf){
-        return inflater.inflate(R.layout.fragment_cursos,container,false);
 
+        view=inflater.inflate(R.layout.fragment_cursos,container,false);
+
+        btnL1=(Button) view.findViewById(R.id.buttonFB);
+
+        btnL1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(), FbL1.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
-
 }
